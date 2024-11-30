b4c86e0e3e4d77b84f1bd5895341c1b291a3354f
audryus <audryus@gmail.com>
2024-11-30 15:27:52 -0300

    remove duplicates

---
+++ remove-duplicates-from-sorted-array/solution.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{0, 0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	removeDuplicates(arr)
+	fmt.Println(arr)
+	//arr2 := []int{3, 3}
+	//removeElement(arr2, 3)
+}
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[j] != nums[i] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+
+	return i + 1
+
+}
